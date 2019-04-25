a723c934290290915ebe5c4cf189bd2cdbf7a50a
germanflighel <flighelmang@gmail.com>
2019-04-25 15:46:03 -0300

    Lee si existe una tabla local

--- LSF/src/LFS.c
+++ LSF/src/LFS.c
@@ -10,15 +10,10 @@
 
 #include "LFS.h"
 #include "sockets.h"
+#include <dirent.h>
 
 int main() {
-	/*
-	 *
-	 *  Estas y otras preguntas existenciales son resueltas getaddrinfo();
-	 *
-	 *  Obtiene los datos de la direccion de red y lo guarda en serverInfo.
-	 *
-	 */
+
 	struct addrinfo hints;
 	struct addrinfo *serverInfo;
 
@@ -30,111 +25,47 @@ int main() {
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
-	hints.ai_flags = AI_PASSIVE;		// Asigna el address del localhost: 127.0.0.1
+	hints.ai_flags = AI_PASSIVE; // Asigna el address del localhost: 127.0.0.1
 	hints.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
 
 	getaddrinfo(NULL, puerto, &hints, &serverInfo); // Notar que le pasamos NULL como IP, ya que le indicamos que use localhost en AI_PASSIVE
 
-	/*
-	 * 	Descubiertos los misterios de la vida (por lo menos, para la conexion de red actual), necesito enterarme de alguna forma
-	 * 	cuales son las conexiones que quieren establecer conmigo.
-	 *
-	 * 	Para ello, y basandome en el postulado de que en Linux TODO es un archivo, voy a utilizar... Si, un archivo!
-	 *
-	 * 	Mediante socket(), obtengo el File Descriptor que me proporciona el sistema (un integer identificador).
-	 *
-	 */
-	/* Necesitamos un socket que escuche las conecciones entrantes */
 	int listenningSocket;
 	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype,
 			serverInfo->ai_protocol);
 
-	/*
-	 * 	Perfecto, ya tengo un archivo que puedo utilizar para analizar las conexiones entrantes. Pero... ������������������Por donde?
-	 *
-	 * 	Necesito decirle al sistema que voy a utilizar el archivo que me proporciono para escuchar las conexiones por un puerto especifico.
-	 *
-	 * 				OJO! Todavia no estoy escuchando las conexiones entrantes!
-	 *
-	 */
 	bind(listenningSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
 	freeaddrinfo(serverInfo); // Ya no lao vamos a necesitar
 
-	/*
-	 * 	Ya tengo un medio de comunicacion (el socket) y le dije por que "telefono" tiene que esperar las llamadas.
-	 *
-	 * 	Solo me queda decirle que vaya y escuche!
-	 *
-	 */
 	listen(listenningSocket, BACKLOG); // IMPORTANTE: listen() es una syscall BLOQUEANTE.
 
 	printf("Esperando memoria... \n");
 
-	/*
-	 * 	El sistema esperara hasta que reciba una conexion entrante...
-	 * 	...
-	 * 	...
-	 * 	BING!!! Nos estan llamando! ������������������Y ahora?
-	 *
-	 *	Aceptamos la conexion entrante, y creamos un nuevo socket mediante el cual nos podamos comunicar (que no es mas que un archivo).
-	 *
-	 *	������������������Por que crear un nuevo socket? Porque el anterior lo necesitamos para escuchar las conexiones entrantes. De la misma forma que
-	 *	uno no puede estar hablando por telefono a la vez que esta esperando que lo llamen, un socket no se puede encargar de escuchar
-	 *	las conexiones entrantes y ademas comunicarse con un cliente.
-	 *
-	 *			Nota: Para que el listenningSocket vuelva a esperar conexiones, necesitariamos volver a decirle que escuche, con listen();
-	 *				En este ejemplo nos dedicamos unicamente a trabajar con el cliente y no escuchamos mas conexiones.
-	 *
-	 */
 	struct sockaddr_in addr; // Esta estructura contendra los datos de la conexion del cliente. IP, puerto, etc.
 	socklen_t addrlen = sizeof(addr);
 
 	int socketCliente = accept(listenningSocket, (struct sockaddr *) &addr,
 			&addrlen);
 
-	/*
-	 * 	Ya estamos listos para recibir paquetes de nuestro cliente...
-	 *		 *
-	 *	Cuando el cliente cierra la conexion, recieve_and_deserialize() devolvera 0.
-	 */
-
 	t_PackagePosta package;
-	int status = 1;		// Estructura que manjea el status de los recieve.
-
-	//t_PackageEnv packageEnvio;
-	//packageEnvio.message = malloc(MAX_MESSAGE_SIZE);
-	//char *serializedPackage;
+	int status = 1;		// Estructura que maneja el status de los recieve.
 
 	printf("Memoria conectada. Esperando Env��o de mensajes.\n");
 
 	while (status) {
 		status = recieve_and_deserialize(&package, socketCliente);
 
-		interpretar_parametros(package.header,package.message);
+		interpretar_parametros(package.header, package.message);
 
-		//fill_package(&packageEnvio, &username);
-
-		// Ver el "Deserializando estructuras dinamicas" en el comentario de la funcion.
 		if (status)
 			printf("Memory says: %s.\n", package.message);
 
-		//serializedPackage = serializarOperandos(&packageEnvio);
-		//send(socketCliente, serializedPackage, packageEnvio.total_size, 0);
-		//dispose_package(&serializedPackage);
 	}
 
 	printf("Cliente Desconectado.\n");
-	/*
-	 * 	Terminado el intercambio de paquetes, cerramos todas las conexiones y nos vamos a mirar Game of Thrones, que seguro nos vamos a divertir mas...
-	 *
-	 *
-	 * 																					~ Divertido es Disney ~
-	 *
-	 */
+
 	close(socketCliente);
 	close(listenningSocket);
-
-	/* See ya! */
 
 	log_destroy(logger);
 	config_destroy(config);
@@ -152,10 +83,9 @@ t_log* iniciar_logger(void) {
 
 }
 
-void interpretar_parametros(int header, char* parametros){
+void interpretar_parametros(int header, char* parametros) {
 
-
-	switch(header){
+	switch (header) {
 
 	case SELECT:
 		lfs_select(parametros);
@@ -167,58 +97,86 @@ void interpretar_parametros(int header, char* parametros){
 }
 
 //Falta agregar funcionalidad de que debe buscar a la tabla correspondiente el valor y demas...
-void lfs_select(char* parametros){
+void lfs_select(char* parametros) {
 
 	t_log* logger_select = iniciar_logger();
-	log_info(logger_select,"Estoy en la instruccion SELECT");
+	log_info(logger_select, "Estoy en la instruccion SELECT");
 
 	char** parametros_separados = string_split(parametros, " ");
 	char* nombre_tabla = malloc(strlen(parametros_separados[0]));
-	strcpy(nombre_tabla,parametros_separados[0]);
+	strcpy(nombre_tabla, parametros_separados[0]);
 
 	int key = atoi(parametros_separados[1]);
 
-	log_info(logger_select,nombre_tabla);
-	log_info(logger_select,string_itoa(key));
+	//1) Verficar que exista la tabla
 
-	free(nombre_tabla);//Aca hice el free por las dudas, pero creo que no deberia hacerlo si lo vamos a usar depues...
+	if (existe_tabla(nombre_tabla)) {
+		log_info(logger_select, "Existe tabla, BRO!");
+		//2) Obtener Metadata
+		//obtener_metadata(nombre_tabla, &metadata);
+		//3) Calcular que particion contiene a KEY
+		//int particionActual = calcular_particion(key, metadata->particiones);
+		//4) Escanear Todas las particiones
+		//valuesEncontrados = encontrar_keys(key, particionActual);
+		//5) Devolver o mostrar el valor mayor
+		//log_info(logger_select, maximoTimestamp(valuesEncontrados));
+	}
+
+	free(nombre_tabla);
 	log_destroy(logger_select);
 
 }
 
-void lfs_insert(char* parametros){ //Falta agregar funcionalidad, solo reconoce los parametros necesarios
+void lfs_insert(char* parametros) { //Falta agregar funcionalidad, solo reconoce los parametros necesarios
 	t_log* logger_insert = iniciar_logger();
-	log_info(logger_insert,"Estoy en la instruccion INSERT");
+	log_info(logger_insert, "Estoy en la instruccion INSERT");
 
 	char** parametros_separados = string_split(parametros, " ");
 
 	char* nombre_tabla = malloc(strlen(parametros_separados[0]));
-	strcpy(nombre_tabla,parametros_separados[0]);
+	strcpy(nombre_tabla, parametros_separados[0]);
 
 	int key = atoi(parametros_separados[1]);
 
-	char* value = malloc(strlen(parametros_separados[2]));
-	strcpy(value, parametros_separados[2]);
+	char* valueAndTimeStamp = malloc(strlen(parametros_separados[2]));
+	strcpy(valueAndTimeStamp, parametros_separados[2]);
 
 	unsigned long timestamp = atoi(parametros_separados[3]);
 
-	log_info(logger_insert,nombre_tabla);
+	log_info(logger_insert, nombre_tabla);
 	log_info(logger_insert, string_itoa(key));
-	log_info(logger_insert, value);
+	log_info(logger_insert, valueAndTimeStamp);
 	log_info(logger_insert, string_itoa(timestamp));
 
+	free(valueAndTimeStamp);
+	free(nombre_tabla);
+
+	log_destroy(logger_insert);
+
+}
+
+int existe_tabla(char* nombre_tabla) {
+	char* ruta = string_new();
+	string_append(&ruta,
+			"/home/utnso/workspace/tp-2019-1c-Ckere/LSF/Debug/tables/");
+	string_append(&ruta, nombre_tabla);
+	DIR *dirp = opendir(ruta);
+	if (dirp == NULL) {
+		return 0;
+	}
+	return 1;
 }
 
 //Preguntar que onda esta opcion, si pierdo la referencia al hacer malloc y devolverlo. Comparar con la otra funcion de abajo
 
 /*char* obtener_nombre_tabla(char** parametros_separados){
-	char* nombre_tabla = malloc(strlen(parametros_separados[0]));
-	strcpy(nombre_tabla,parametros_separados[0]);
-	return nombre_tabla;
-}
-*/
+ char* nombre_tabla = malloc(strlen(parametros_separados[0]));
+ strcpy(nombre_tabla,parametros_separados[0]);
+ return nombre_tabla;
+ }
+ */
 
 /*void obtener_nombre_tabla(char* nombre_tabla, char** parametros_separados){
-	nombre_tabla = malloc(strlen(parametros_separados[0]));
-	strcpy(nombre_tabla,parametros_separados[0]);
-}*/
+ nombre_tabla = malloc(strlen(parametros_separados[0]));
+ strcpy(nombre_tabla,parametros_separados[0]);
+ }*/
